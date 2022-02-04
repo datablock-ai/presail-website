218cf666f336674426266bf017ab9cf59a10f00a
Bas <76127152+bas-db@users.noreply.github.com>
2022-02-04 15:51:34 +0100

    Remove tiers

--- pages/pricing.js
+++ pages/pricing.js
@@ -18,7 +18,7 @@ export default function Pricing() {
         <Header />
         <PricingHero />
         <PricingCTA />
-        <Tiers />
+        
         <Faq />
         <Footer />
       </div>
