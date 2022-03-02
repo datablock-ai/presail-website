d2824b2033c3728ad350ca9e833b8c3011e222b2
Bas <76127152+bas-db@users.noreply.github.com>
2022-03-02 17:16:13 +0100

    update banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
 
 export default function Example() {
@@ -7,12 +6,12 @@ export default function Example() {
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <span className="flex p-2 rounded-lg bg-indigo-800">
+            <span className="flex p-2 rounded-lg bg-indigo-400">
               <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </span>
             <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">We announced a new product!</span>
-              <span className="hidden md:inline">We're excited to announce our $1.6M seed-stage</span>
+              <span className="md:hidden">Presail raises $1.6 million seed investment round</span>
+              <span className="hidden md:inline">Presail announces $1.6 million seed investment round, led by Skyfall Ventures</span>
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
@@ -21,8 +20,17 @@ export default function Example() {
               target="_blank"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
-              Read our blog
+              Learn more
             </a>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+            >
+              
+              
+            </button>
           </div>
         </div>
       </div>
