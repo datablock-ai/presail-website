85a4b4a1fdd0c1558337de6b487c4bab8cadba22
Bas <76127152+bas-db@users.noreply.github.com>
2022-03-02 17:54:41 +0100

    decrease banner size

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
 export default function Example() {
   return (
     <div className="bg-indigo-400">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-1 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
             <span className="flex p-2 rounded-lg bg-indigo-400">
