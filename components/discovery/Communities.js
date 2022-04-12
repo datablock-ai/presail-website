d404ad9f47a27918299a763dd911783ac1c96d6e
Bas <76127152+bas-db@users.noreply.github.com>
2022-04-12 17:47:00 +0200

    Update communities page

--- components/discovery/Communities.js
+++ components/discovery/Communities.js
@@ -1,12 +1,11 @@
-import { MailIcon, ChatIcon, BadgeCheckIcon } from '@heroicons/react/solid'
+import { MailIcon, ExternalLinkIcon, BadgeCheckIcon } from '@heroicons/react/solid'
 
 const communities = [
   {
     name: 'DCI',
-    description: 'Paradigm Representative',
+    description: 'We support start-ups in the crypto space with capital, marketing and strategic partnerships.',
     role: 'Verified',
     email: 'janecooper@example.com',
-    telegram: 'https://t.me/',
     website: 'https://google.com',
     imageUrl: '/clients/dutch-crypto-logo.webp'
   },
@@ -15,7 +14,6 @@ const communities = [
     description: 'We make private investments available to everyone.',
     role: 'Verified',
     email: 'hello@legion.ventures',
-    telegram: 'https://t.me/',
     website: 'https://legion.ventures/',
     imageUrl: '/clients/legion-ventures-logo2.webp'
   },
@@ -24,7 +22,6 @@ const communities = [
     description: 'A blockchain advisory, investment and education firm.',
     role: 'Verified',
     email: 'contact@vespertine.capital',
-    telegram: 'https://t.me/',
     website: 'https://www.vespertine.capital/',
     imageUrl: '/clients/vespertine-logo.webp'
   },
@@ -33,7 +30,6 @@ const communities = [
     description: 'The Duck community is different from anything the financial world has seen before.',
     role: 'Verified',
     email: 'janecooper@example.com',
-    telegram: 'https://t.me/',
     website: 'https://duckdao.io/',
     imageUrl: '/clients/duckdao-logo.webp'
   },
@@ -42,7 +38,6 @@ const communities = [
     description: 'Ferrum Network is a pioneer in ushering in the era of Interoperability 2.0.',
     role: 'Verified',
     email: 'janecooper@example.com',
-    telegram: 'https://t.me/',
     website: 'https://ferrum.network/',
     imageUrl: '/clients/ferrum-logo.webp'
   },
@@ -53,14 +48,14 @@ export default function Example() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
-        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
           {communities.map((community) => (
             <li
               key={community.email}
               className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
             >
               <div className="flex-1 flex flex-col p-8">
-                <img className="flex-shrink-0 mx-auto invert" src={community.imageUrl} alt={community.name} />
+                <img className="h-20 flex-shrink-0 mx-auto invert" src={community.imageUrl} alt={community.name} />
                 <h3 className="mt-6 text-gray-900 text-sm font-medium">{community.name}</h3>
                 <dl className="mt-1 flex-grow flex flex-col justify-between">
                   <dt className="sr-only">description</dt>
@@ -71,9 +66,6 @@ export default function Example() {
                       <BadgeCheckIcon className="w-5 h-5 text-main inline-flex" aria-hidden="true" />
                       <span>{community.role}</span>
                     </span>
-                  </dd>
-                  <dd className="mt-3">
-                    <a href={community.website} className="text-xs" target="_blank">Visit website</a>
                   </dd>
                 </dl>
               </div>
@@ -90,11 +82,11 @@ export default function Example() {
                   </div>
                   <div className="-ml-px w-0 flex-1 flex">
                     <a
-                      href={community.telegram}
+                      href={community.website}
                       className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
                     >
-                      <ChatIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                      <span className="ml-3">Telegram</span>
+                      <ExternalLinkIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
+                      <span className="ml-3">Website</span>
                     </a>
                   </div>
                 </div>
