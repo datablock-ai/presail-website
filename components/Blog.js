9c8760bb6a1c93d0c4a3242f162f9fb0b8954ddf
Bas <76127152+bas-db@users.noreply.github.com>
2022-06-20 10:25:56 +0200

    Update blog cta

--- components/Blog.js
+++ components/Blog.js
@@ -62,7 +62,7 @@ export default function Example() {
         <div className="text-center">
           <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">From the blog</h2>
           <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-            Investing should be for everyone. Not just the rich and well-connected.
+            Read what's going on behind the scenes at Presail
           </p>
         </div>
         <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
