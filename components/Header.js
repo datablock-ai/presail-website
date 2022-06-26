fd47bde68def3e87e57c86204e55314164f3f77e
Bas <76127152+bas-db@users.noreply.github.com>
2022-06-26 15:51:44 +0200

    bamp it

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,10 @@ import {
   ChartBarIcon,
   FireIcon,
   LockClosedIcon,
+  ClipboardListIcon,
   MenuIcon,
+  ChatIcon,
+  BookmarkIcon,
   XIcon,
   ChevronDoubleRightIcon,
   CurrencyDollarIcon,
@@ -13,6 +16,20 @@ import {
 } from '@heroicons/react/outline'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
+const resources = [
+  {
+    name: 'SAFT Agreement',
+    description: 'The SAFT is the commercial instrument used to convey rights in tokens prior to the development of the tokens’ functionality.',
+    href: 'https://saftproject.com/',
+    icon: ClipboardListIcon,
+  },
+  {
+    name: 'Due diligence checklist',
+    description: 'How to evaluate projects. A checklist for running a diligence process for an early-stage crypto investment.',
+    href: 'https://saftproject.com/',
+    icon: BookmarkIcon,
+  },
+]
 const features = [
   {
     name: 'KYC & AML',
@@ -68,7 +85,7 @@ export default function Example() {
           </div>
           <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
             <Popover.Group as="nav" className="flex space-x-10">
-             <Popover>
+              <Popover>
                 {({ open }) => (
                   <>
                     <Popover.Button
@@ -103,6 +120,81 @@ export default function Example() {
                             <a
                               key={item.name}
                               className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-secondary group"
+                            >
+                              <div className="flex md:h-full lg:flex-col">
+                                <div className="flex-shrink-0">
+                                  <span className="inline-flex items-center justify-center h-10 w-10 rounded-md bg-secondary text-deep sm:h-12 sm:w-12">
+                                    <item.icon className="h-6 w-6" aria-hidden="true" />
+                                  </span>
+                                </div>
+                                <div className="ml-4 md:flex-1 md:flex md:flex-col md:justify-between lg:ml-0 lg:mt-4">
+                                  <div>
+                                    <p className="text-base font-medium text-deep group-hover:text-deep">{item.name}</p>
+                                    <p className="mt-1 text-sm text-subgray group-hover:text-deep">{item.description}</p>
+                                  </div>
+                                </div>
+                              </div>
+                            </a>
+                            </Link>
+                          ))}
+                        </div>
+                        <div className="bg-secondary">
+                          <div className="max-w-7xl mx-auto space-y-6 px-4 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-6 lg:px-8">
+                            {callsToAction.map((item) => (
+                              <div key={item.name} className="flow-root">
+                                <a
+                                  href={item.href}
+                                  target="_blank"
+                                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-deep bg-deep text-white"
+                                >
+                                  <item.icon className="flex-shrink-0 h-6 w-6 text-deep text-white" aria-hidden="true" />
+                                  <span className="ml-3">{item.name}</span>
+                                </a>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+             <Popover>
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? 'text-deep' : 'text-deep',
+                        'group bg-white rounded-md inline-flex items-center text-base font-medium focus:outline-none'
+                      )}
+                    >
+                      <span>Resources</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? 'text-main' : 'text-main',
+                          'ml-2 h-5 w-5'
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 -translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 -translate-y-1"
+                    >
+                      <Popover.Panel className="hidden md:block absolute z-10 top-full inset-x-0 transform shadow-lg bg-white">
+                        <div className="max-w-7xl mx-auto grid gap-y-6 px-4 py-6 sm:grid-cols-2 sm:gap-8 sm:px-6 sm:py-8 lg:grid-cols-4 lg:px-8 lg:py-12 xl:py-16">
+                          {resources.map((item) => (
+                            <Link href={item.href}>
+                            <a
+                              key={item.name}
+                              className="-m-3 p-3 flex flex-col justify-between rounded-lg hover:bg-secondary group"
+                              target="_blank"
                             >
                               <div className="flex md:h-full lg:flex-col">
                                 <div className="flex-shrink-0">
